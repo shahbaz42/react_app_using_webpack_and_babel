38d6728b8efd6a3d890d27abe08e124d50f1962e
Shahbaz <shahbaz_ali@outlook.in>
2023-07-14 01:51:37 +0530

    add index.js code and App.js code

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () =>{
+    return (
+        <h1>
+            React App built from scratch with Webpack and Babel
+        </h1>
+    )
+}
+
+export default App;
